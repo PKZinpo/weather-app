6052fc5a71085e588bbc9b6a41725e7ef5c206b6
PKZinpo <mbquach@gmail.com>
2022-07-18 19:19:40 -0500

    init commit

---
+++ src/components/Background.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Background() {
+
+    return (
+        <div className='background'>
+            <div className='box'/>
+            <a className='credit' href="https://www.freepik.com/vectors/mountain-river">Mountain river vector created by jcomp - www.freepik.com</a>
+        </div>
+    );
+}
